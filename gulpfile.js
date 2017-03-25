3bc42104283bbeede32542856e6ea37287fb7c90
Tamas Magedli <tamas.magedli@gmail.com>
2017-03-25 15:34:06 +0100

    Improve logging of lerna

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 
 require('./gulp/generate-package-files');
 
+const chalk = require('chalk');
 const gulp = require('gulp');
-const shell = require('gulp-shell');
 const tools = require('urbanjs-tools');
 
 tools.initializeTasks(gulp, {
@@ -12,5 +12,32 @@ tools.initializeTasks(gulp, {
 });
 
 gulp.task('pre-release', ['generate-package-files', 'retire', 'nsp'], (done) => {
-  shell.task([`lerna bootstrap --hoist`])(done);
+  // disable progress bar
+  const lernaProgressBar = require("lerna/lib/progressBar");
+  lernaProgressBar.tick = () => {
+  };
+
+  // filter debug messages
+  let semafor = 0;
+  const lernaLogger = require("lerna/lib/logger");
+  lernaLogger._formatValue = (arg) => typeof arg === 'string' ? arg : '';
+  lernaLogger._emit = (message) => {
+    // allow not debug messages
+    if (message.slice(0, 5) !== '\u001b[94m') {
+      console.log(message);
+    }
+
+    const index = message.indexOf('> gulp pre-release');
+    if (index > 0 && /NpmUtilities\.runScriptInDir/.test(message)) {
+      console.log(chalk[semafor++ % 2 ? 'gray' : 'cyan'](message.slice(index)));
+    }
+
+    if (/Successfully bootstrapped \d+ packages/.test(message)) {
+      done();
+    }
+  };
+
+  // run bootstrap command
+  process.argv = process.argv.slice(0, 2).concat('bootstrap', '--hoist', '--loglevel=verbose');
+  require('lerna/bin/lerna');
 });
